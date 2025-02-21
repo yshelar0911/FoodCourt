f88166ffba8025e6bde91cc8ac9c60649521f00c
yshelar0911 <yuvrajshelar2000@gmail.com>
2025-02-21 10:42:15 +0530

    add swagger configuration

--- FoodCourt/Program.cs
+++ FoodCourt/Program.cs
@@ -1,36 +1,56 @@
 
+using Microsoft.AspNetCore;
+using Microsoft.Identity.Client;
+
 namespace FoodCourt
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            #region
+            //var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            //// Add services to the container.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            //builder.Services.AddControllers();
+            //// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            //builder.Services.AddEndpointsApiExplorer();
+            //builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+            //var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+            //// Configure the HTTP request pipeline.
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+
+            //app.UseHttpsRedirection();
+
+            //app.UseAuthorization();
+
+
+            //app.MapControllers();
+
+            //app.Run();
+            #endregion
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        //public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+        //    { 
+        //        webBuilder.UseStartup<Startup>();
+        //    });
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)=>
+          WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
     }
-}
+
+
+
+
---
+++ FoodCourt/Startup.cs
@@ -0,0 +1,56 @@
+﻿
+using FC.Database.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+
+namespace FoodCourt
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddCors(option =>
+            {
+                option.AddPolicy("AllowAll", policy => policy
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+          services.AddMvc(option =>option.EnableEndpointRouting = false).AddNewtonsoftJson(Options => Options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+            services.AddOptions();
+            services.AddLogging();
+            services.AddHttpContextAccessor();
+            services.AddHttpClient();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        services.AddDbContext<QuickKartDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("QuickKartDBConnectionString")));
+
+            //services.AddEndpointsApiExplorer();
+            //services.AddSwaggerGen();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors("AllowAll");
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
