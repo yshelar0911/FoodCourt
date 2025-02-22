1de49654fc9ea5c37e6687b04e7c40b5ee16c79c
yshelar0911 <yuvrajshelar2000@gmail.com>
2025-02-22 21:07:51 +0530

    Update Program.cs

--- FoodCourt/Program.cs
+++ FoodCourt/Program.cs
@@ -11,12 +11,7 @@ namespace FoodCourt
            CreateWebHostBuilder(args).Build().Run();
         }
 
-        //public static IHostBuilder CreateWebHostBuilder(string[] args) =>
-        //    Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
-        //    { 
-        //        webBuilder.UseStartup<Startup>();
-        //    });
-
+ 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)=>
           WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
             }
