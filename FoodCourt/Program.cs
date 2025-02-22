ed896c75c2960d51b9c229bf4d07a3a9399a4a57
yshelar0911 <yuvrajshelar2000@gmail.com>
2025-02-22 21:06:30 +0530

    remove unwanted code

--- FoodCourt/Program.cs
+++ FoodCourt/Program.cs
@@ -8,35 +8,7 @@ namespace FoodCourt
     {
         public static void Main(string[] args)
         {
-            #region
-            //var builder = WebApplication.CreateBuilder(args);
-
-            //// Add services to the container.
-
-            //builder.Services.AddControllers();
-            //// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            //builder.Services.AddEndpointsApiExplorer();
-            //builder.Services.AddSwaggerGen();
-
-            //var app = builder.Build();
-
-            //// Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
-            //    app.UseSwagger();
-            //    app.UseSwaggerUI();
-            //}
-
-            //app.UseHttpsRedirection();
-
-            //app.UseAuthorization();
-
-
-            //app.MapControllers();
-
-            //app.Run();
-            #endregion
-            CreateWebHostBuilder(args).Build().Run();
+           CreateWebHostBuilder(args).Build().Run();
         }
 
         //public static IHostBuilder CreateWebHostBuilder(string[] args) =>
